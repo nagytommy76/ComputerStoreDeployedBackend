1cff16330de08f4bd6e3038eca1e2336bf7bf0a2
nagytommy76 <nagytommy76@gmail.com>
2022-11-12 16:45:46 +0100

    Modified the package.json

--- config/db.js
+++ config/db.js
@@ -1,23 +1,26 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const endpoints_config_1 = require("./endpoints.config");
+'use strict'
+var __importDefault =
+   (this && this.__importDefault) ||
+   function (mod) {
+      return mod && mod.__esModule ? mod : { default: mod }
+   }
+Object.defineProperty(exports, '__esModule', { value: true })
+const mongoose_1 = __importDefault(require('mongoose'))
+const endpoints_config_1 = require('./endpoints.config')
 const connectDB = async () => {
-    try {
-        mongoose_1.default.set('useCreateIndex', true);
-        const connection = await mongoose_1.default.connect(endpoints_config_1.DB_CONNECTION, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-        });
-        console.log(`MongoDB connected: ${connection.connection.host}`);
-    }
-    catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-};
-exports.default = connectDB;
+   try {
+      mongoose_1.default.set('useCreateIndex', true)
+      const connection = await mongoose_1.default.connect(endpoints_config_1.DB_CONNECTION, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useFindAndModify: false,
+      })
+      console.log(`MongoDB connected: ${connection.connection.host}`)
+   } catch (error) {
+      console.error(error)
+      process.exit(1)
+   }
+}
+exports.default = connectDB
+// Ha nem jó a mongoDB connection string-et
+// https://github.com/prisma/prisma/issues/10287
